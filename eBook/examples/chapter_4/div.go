9beee64fa6a1501922ff87dbc8d3ef099a59c6d8
bwangel23 <bwangel.me@gmail.com>
2016-12-20 10:30:18 +0800

    feat(4): add datatype

---
+++ eBook/examples/chapter_4/div.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	a, b := 3.0, 0.0
+	c := a / b
+
+	fmt.Println(c)
+}
---
+++ eBook/examples/chapter_4/ex-4-5.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+	type NAME string
+
+	var name NAME = "michael"
+
+	println(name)
+}
