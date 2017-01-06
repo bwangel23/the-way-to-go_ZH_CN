27c7f34c773d61a05f12763878009031ce891b5a
bwangel23 <bwangel.me@gmail.com>
2017-01-06 09:52:48 +0800

    feat(7): add byte array init

--- eBook/exercises/chapter_7/ex7.go
+++ eBook/exercises/chapter_7/ex7.go
@@ -6,14 +6,20 @@ import (
 )
 
 func main() {
+	var byteArray = [...]byte{'c', 'f', 'b'}
+	s := string(byteArray[:])
+	fmt.Printf("%s\n", s)
+}
+
+func splitBuffer() {
 	var str string = "hello, xff"
 	var buf *bytes.Buffer = bytes.NewBufferString(str)
-	s1, s2 := splitBuffer(buf)
+	s1, s2 := splitBufferFunc(buf)
 	fmt.Println(s1, s2)
 }
 
-func splitBuffer(buf *bytes.Buffer) (s1 []byte, s2 []byte) {
-	s1 = buf.ReadBytes(3)
-	s2 = buf.ReadByte()
+func splitBufferFunc(buf *bytes.Buffer) (s1 []byte, s2 []byte) {
+	// s1 = buf.ReadBytes(3)
+	// s2 = buf.ReadByte()
 	return s1, s2
 }
