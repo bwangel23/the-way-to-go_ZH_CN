f20be699e5fcf36d8aeb4157e8799aec631ee44c
bwangel23 <bwangel.me@gmail.com>
2017-01-06 12:32:56 +0800

    feat(7): finish 7.5

--- eBook/exercises/chapter_7/ex7.go
+++ eBook/exercises/chapter_7/ex7.go
@@ -6,7 +6,76 @@ import (
 )
 
 func main() {
-	splitBuffer()
+	// splitBuffer()
+	// forRange()
+	// arraySumFunc()
+	// arrayAppend()
+	// sliceCopy()
+	InsertStringSliceFunc()
+}
+
+func InsertStringSliceFunc() {
+	dest := [...]byte{'a', 'b', 'c'}
+	src := [...]byte{'e', 'f', 'd'}
+	fmt.Println(string(InsertStringSlice(dest[:], src[:], 1)))
+}
+
+func InsertStringSlice(dest []byte, src []byte, index int) (result []byte) {
+
+	// 复制的时候一定要确保目的切片足够长
+	// result = make([]byte, 10, 20)
+	// copy(result, dest[:index])
+
+	result = append(result, dest[:index]...)
+	result = append(result, src...)
+	result = append(result, dest[index:]...)
+
+	return
+}
+
+func sliceCopy() {
+	var sliceFrom = []int{1, 2, 3}
+	var sliceTo = make([]int, len(sliceFrom), 10)
+
+	copy(sliceTo, sliceFrom[:1])
+
+	fmt.Println(sliceTo)
+}
+
+func arrayAppend() {
+	// 上面的定义是一个数组，下面的定义是一个切片
+	// var sl1 = [...]int{0, 1, 3, 4}
+	var sl1 = []int{1, 2, 3}
+
+	sl1 = append(sl1, 5, 8)
+
+	fmt.Println(sl1)
+}
+
+func forRange() {
+	var item int
+	var items = [...]int{10, 20, 30, 40, 50}
+
+	for _, item = range items {
+		item *= 2
+	}
+
+	fmt.Println(item)
+}
+
+func arraySumFunc() {
+	// 这里使用了数组指针
+	var arrayP = &[...]int{1, 3, 4, 5, 6, 7, 9}
+
+	fmt.Println(arraySum(arrayP))
+}
+
+func arraySum(array *[7]int) (sum int) {
+	for _, value := range *array {
+		sum += value
+	}
+
+	return
 }
 
 func splitBuffer() {
